053284c04867fc2638d5fccd7e739c543e120957
Francisco Briceno <fbriceno@gmail.com>
2013-04-22 04:09:28 -0400

    a

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -15,62 +15,10 @@
  * under the License.
  */
 
-require '/home/ubuntu/test/web/src/facebook.php';
+//require '/home/ubuntu/test/web/src/facebook.php';
 
 // Create our Application instance (replace this with your appId and secret).
-$facebook = new Facebook(array(
-  'appId'  => '163207260491691',
-  'secret' => '0d45ef52a848c029298c462209ee212c',
-));
 
-// Get User ID
-$user = $facebook->getUser();
-
-// We may or may not have this data based on whether the user is logged in.
-//
-// If we have a $user id here, it means we know the user is logged into
-// Facebook, but we don't know if the access token is valid. An access
-// token is invalid if the user logged out of Facebook.
-
-if ($user) {
-  try {
-    // Proceed knowing you have a logged in user who's authenticated.
-    $user_profile = $facebook->api('/me');
-    $myFriends = $facebook->api('/me/friends');
-    
-  } catch (FacebookApiException $e) {
-    error_log($e);
-    $user = null;
-  }
-}
-
-// Login or logout url will be needed depending on current user state.
-if ($user) {
-  $logoutUrl = $facebook->getLogoutUrl();
-} else {
-  //$loginUrl = $facebook->getLoginUrl();
- $params = array(
-    //"redirect_uri" => REDIRECT_URI,
-    "scope" => "email,read_stream,publish_stream,user_photos,user_videos");
-    //echo '<a href="' . $fb->getLoginUrl($params) . '">Login</a>';
-	echo '<a href="' . $loginUrl = $facebook->getLoginUrl($params) . '">Login</a>';
-
-}
-$signed_request = $facebook->getSignedRequest();
-$page_id = $signed_request["page"]
-["id"];
-$page_admin = $signed_request["page"]
-["admin"];
-$like_status = $signed_request["page"]
-["liked"];
-$country = $signed_request["user"]
-["country"];
-$locale = $signed_request["user"]
-["locale"];
-
-
-// This call will always work since we are fetching public data.
-$naitik = $facebook->api('/fbricenop');
 
 ?>
 <!doctype html>
@@ -92,7 +40,11 @@ $naitik = $facebook->api('/fbricenop');
   </head>
   <body>
     <h1>php-sdk</h1>
+<div id="contenedor">
 
+<?php  echo $sf_content ?>
+
+</div><!--/contenedor-->
     <?php if ($user): ?>
       <a href="<?php echo $logoutUrl; ?>">Logout</a>
     <?php else: ?>
