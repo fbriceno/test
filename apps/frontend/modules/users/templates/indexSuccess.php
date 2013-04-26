5603e0b4326e1415043408823c1889eed00a9e4b
Francisco Briceno <fbriceno@gmail.com>
2013-04-26 17:10:31 -0400

    a

--- apps/frontend/modules/users/templates/indexSuccess.php
+++ apps/frontend/modules/users/templates/indexSuccess.php
@@ -335,10 +335,16 @@ if ($userf) {
      }
 	 }else{ ?>
 	    <div id="fb-root"></div>
-        <script src="http://connect.facebook.net/en_US/all.js"></script>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1&appId=163207260491691";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 	    <div align="center">
         <h3> Imagen de promocion no se entra hasta hacer me gusta </h3>
-		<div class="fb-like"></div>
+		<div class="fb-like" data-href="http://www.ecabdo.cl" data-send="true" data-width="450" data-show-faces="true"></div
 		<img id="image"/>
         <div id="name"></div>
       </div> <?
