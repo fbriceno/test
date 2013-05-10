2a342d977dec99843171c412401b7c071cb29b11
Francisco Briceno <fbriceno@gmail.com>
2013-05-10 00:38:04 -0400

    a

--- apps/frontend/modules/users/templates/indexSuccess.php
+++ apps/frontend/modules/users/templates/indexSuccess.php
@@ -31,7 +31,7 @@ if ($userf) {
 //));
  $params = array(
     //"redirect_uri" => REDIRECT_URI,
-    "scope" => "email,read_stream,publish_stream,user_photos,user_videos,user_birthday,user_checkins,user_groups,user_status,friends_birthday");
+    "scope" => "email,read_stream,publish_stream,user_birthday,user_checkins,user_groups,user_likes,user_status,friends_birthday");
     //echo '<a href="' . $fb->getLoginUrl($params) . '">Login</a>';
 	?><div id="fb-root"></div>
 <script>(function(d, s, id) {
