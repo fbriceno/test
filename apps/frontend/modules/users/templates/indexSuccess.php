e33c0a6aa3aecf37aaeceb41df0ac37f1570308b
Francisco Briceno <fbriceno@gmail.com>
2013-05-10 01:16:12 -0400

    a

--- apps/frontend/modules/users/templates/indexSuccess.php
+++ apps/frontend/modules/users/templates/indexSuccess.php
@@ -1,34 +1,19 @@
 ﻿<?php
 require '/home/ubuntu/test/web/src/facebook.php';
-	$this->userss = UsersPeer::doSelect(new Criteria());
-	// global $log;
-//$logPath = sfConfig::get('sf_log_dir').'/llamar.log';
-//$log = new sfFileLogger(new sfEventDispatcher(), array('level' => sfFileLogger::DEBUG,'file' => $logPath,'type' => 'llamar'));
-  //ini_set('display_errors',1);
+ //ini_set('display_errors',1);
   $facebook = new Facebook(array(
   'appId'  => '163207260491691',
   'secret' => '0d45ef52a848c029298c462209ee212c',
 ));
 
-// Get User ID
 $userf = $facebook->getUser();
-
-// We may or may not have this data based on whether the user is logged in.
-//
-// If we have a $user id here, it means we know the user is logged into
-// Facebook, but we don't know if the access token is valid. An access
-// token is invalid if the user logged out of Facebook.
-
-// Login or logout url will be needed depending on current user state.
+/******** MOSTRAR LOGIN *******/
 if ($userf) {
+/******* LOGEADO *************/
   $logoutUrl = $facebook->getLogoutUrl();
 } else {
-  //$loginUrl = $facebook->getLoginUrl();
-  //***FULL ACCESS 
-  //$loginUrl = $facebook->getLoginUrl(
-//array( 
-//'scope' => 'user_about_me,user_activities,user_birthday,user_checkins,user_education_history,user_events,user_groups,user_hometown,user_interests,user_likes,user_location,user_notes,user_online_presence,user_photo_video_tags,user_photos,user_relationships,user_relationship_details,user_religion_politics,user_status,user_videos,user_website,user_work_history,email,read_friendlists,read_insights,read_mailbox,read_requests,read_stream,xmpp_login,ads_management,create_event,manage_friendlists,manage_notifications,offline_access,publish_checkins,publish_stream,rsvp_event,sms,publish_actions,manage_pages'
-//));
+/******* SIN LOGEAR **********/
+ //'scope' => 'user_about_me,user_activities,user_birthday,user_checkins,user_education_history,user_events,user_groups,user_hometown,user_interests,user_likes,user_location,user_notes,user_online_presence,user_photo_video_tags,user_photos,user_relationships,user_relationship_details,user_religion_politics,user_status,user_videos,user_website,user_work_history,email,read_friendlists,read_insights,read_mailbox,read_requests,read_stream,xmpp_login,ads_management,create_event,manage_friendlists,manage_notifications,offline_access,publish_checkins,publish_stream,rsvp_event,sms,publish_actions,manage_pages'
  $params = array(
     //"redirect_uri" => REDIRECT_URI,
     "scope" => "email,read_stream,publish_stream,user_birthday,user_checkins,user_groups,user_likes,user_interests,user_status,friends_birthday");
@@ -100,10 +85,8 @@ if ($userf) {
     echo $e;
 	$userf = null;
   }
+  
 }
-// echo ' <div>'
-
-
 
 
 $signed_request = $facebook->getSignedRequest();
@@ -324,24 +307,6 @@ echo $locale;
 	 
 	 
      }
-	 
-	 
-	 // LISTA DE AMIGOS
-	
-	//print_r($myFriends);
-    /*foreach ($myFriends['data'] as $friend) 
-    {
-	  echo '<img src="https://graph.facebook.com/'.$friend['id']. '/picture">';
-      echo '<li style="display:inline;"><fb:profile-pic uid="'.$friend['id'].'" width="32" height="32" linked="true" /></li>';
-    }
-    echo "</ul><br/><br/>";*/
-	/*$myInvited = $facebook->api(array(  
-    'method' => 'fql.query',  
-    'query' => 'SELECT uid FROM friendlist_member WHERE flid IN (SELECT flid FROM friendlist WHERE owner=me()  )'  
-));  */
-	
-	
-	
 	?>
 	<div id="portada">
   <p>DREAMGIRLS TE REGALA UN LED 32’.<br />
@@ -355,13 +320,6 @@ echo $locale;
    <div id="fb-root"></div>
     <script src="http://connect.facebook.net/en_US/all.js"></script>
    <div>
-   <p>
-      <!--<input type="button"
-        onclick="sendRequestToRecipients(); return false;"
-        value="Send Request to Users Directly"
-      />
-      <input type="text" value="User ID" name="user_ids" /> -->
-      </p>
     <p>
     <input type="button"
       onclick="sendRequestViaMultiFriendSelector(); return false;"
@@ -482,26 +440,6 @@ Volver a la página de la promoción aquí.<br />
    </p>
 	</div>
 	<?php
-//  }else {
-
- 
-
-         /*$app_id = "163207260491691";
-
-         $canvas_page = "http://apps.facebook.com/concursoejemplo";
-
-         $message = "Would you like to join me in this great app?";
-
-         $requests_url = "http://www.facebook.com/dialog/apprequests?app_id=" 
-                . $app_id . "&redirect_uri=" . urlencode($canvas_page)
-                . "&message=" . $message;
-
-         if (empty($_REQUEST["request_ids"])) {
-            echo("<script> top.location.href='" . $requests_url . "'</script>");
-         } else {
-            echo "Request Ids: ";
-            print_r($_REQUEST["request_ids"]);
-         }*/
 	 
 	 }else{ ?>
 	    
